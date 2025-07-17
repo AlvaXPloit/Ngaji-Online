cf46601be74398fcb7c81c2687c94878e97b6d08
AlvaXPloit <alvaxploit@gmail.com>
2025-07-18 03:15:46 +0700

    Add files via upload

---
+++ assets/quotes-doa.js
@@ -0,0 +1,24 @@
+const islamicData = {
+  quotes: [
+    "Sesungguhnya bersama kesulitan ada kemudahan. (QS. Al-Insyirah: 6)",
+    "Janganlah kamu berputus asa dari rahmat Allah. (QS. Az-Zumar: 53)",
+    "Shalat adalah tiang agama.",
+    "Allah tidak membebani seseorang melainkan sesuai kesanggupannya. (QS. Al-Baqarah: 286)",
+    "Berdoalah kepada-Ku, niscaya akan Aku kabulkan. (QS. Ghafir: 60)",
+    "Tidak ada musibah yang menimpa kecuali dengan izin Allah. (QS. At-Taghabun: 11)",
+    "Allah Maha Pengampun lagi Maha Penyayang.",
+    "Orang terbaik adalah yang belajar dan mengajarkan Al-Qur'an.",
+    "Hanya dengan mengingat Allah hati menjadi tenang. (QS. Ar-Ra’d: 28)",
+    "Sebaik-baik manusia adalah yang paling bermanfaat bagi manusia lain."
+  ],
+  doa: [
+    "رَبِّ زِدْنِي عِلْمًا",
+    "اللّهُمَّ اجْعَلْنِيْ مِنَ التَّوَّابِيْنَ",
+    "اللَّهُمَّ اهْدِنِي فِيمَنْ هَدَيْتَ",
+    "رَبِّ اغْفِرْ لِي وَلِوَالِدَيَّ",
+    "اللَّهُمَّ إِنِّي أَسْأَلُكَ عِلْمًا نَافِعًا",
+    "اللَّهُمَّ اجْعَلْنِيْ مِنَ الْمُتَطَهِّرِيْنَ",
+    "اللَّهُمَّ ثَبِّتْ قَلْبِي عَلَى دِينِكَ",
+    "اللَّهُمَّ أَصْلِحْ لِي دِينِيَ الَّذِي هُوَ عِصْمَةُ أَمْرِي"
+  ]
+};
